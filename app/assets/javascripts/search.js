3c8d9a0a175fd14c5f8431dc9ce4659f6d10bc10
Le Thi Cuc <le.thi.cuc@framgia.com>
2016-10-10 16:05:23 +0700

    Filter word by category

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,6 @@
+$(document).on('change', '#category_id', function() {
+  $('#search').val($('#category_id').val());
+  $.get($('#category_search').attr('action'), $('#category_search').serialize(),
+    null, 'script');
+  return false;
+});
