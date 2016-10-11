16a702155b0b539132f772f21d39789793179df4
phamxuanvu <xuanvupham96@gmail.com>
2016-10-11 12:56:00 +0700

    Admin create question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,9 @@ class Question < ApplicationRecord
   belongs_to :category
   has_many :answers
   has_many :results
+  validates :name, presence: true
   has_many :lessons, through: :results
 
   scope :random, ->{order "RANDOM()"}
+  accepts_nested_attributes_for :answers, allow_destroy: true
 end
